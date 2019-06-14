452f066e058945dea708beda1fa999e2f1618a6c
Reed Van Anrooy <reed.vananrooy@gmail.com>
2019-06-14 15:52:16 -0700

    Adds photo attribute to posts and renders to home page

--- components/Post/Post.js
+++ components/Post/Post.js
@@ -1,11 +1,32 @@
 import React from 'react';
 
 import {
-  Text
+  Text,
+  Image,
+  View,
+  StyleSheet
 } from 'react-native';
 
 export default class Post extends React.Component {
   render() {
-    return (<Text>{this.props.caption}</Text>);
+    return (
+      <View>
+        <Image
+            style={styles.photo}
+            source={{uri: this.props.photo}}
+          />
+        <Text style={styles.caption}>{this.props.caption}</Text>
+      </View>
+    );
   }
 }
+
+const styles = StyleSheet.create({
+  photo: {
+    width: "100%",
+    height: 200
+  },
+  caption: {
+    margin: 10,
+  }
+});
--- components/PostFeed/PostFeed.js
+++ components/PostFeed/PostFeed.js
@@ -11,7 +11,7 @@ export default class PostFeed extends React.Component {
 
   render() {
     let posts = (this.props.posts ? this.props.posts.map((post, key) =>
-      <Post key={post.id} caption={post.caption} charity={post.charity_id} project={post.project_id} />
+      <Post key={post.id} photo={post.photo} caption={post.caption} charity={post.charity_id} project={post.project_id} />
     ) : []);
 
     return (<View>{posts}</View>);
